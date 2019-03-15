440ad421575e9fd5dbd692696f88b4d7a1e20530
Mario Windsor <mario@lazaro.in>
2019-03-15 17:09:30 +0530

    Injest Markup from Document

--- pages/home.php
+++ pages/home.php
@@ -23,6 +23,308 @@
 
 
 
+[ LOGO ]
+Real-time Pricing
+Location
+Call 9663396979
+Enquire Now
+
+
+
+
+Secretly 
+Kickass 
+Standalone 
+Houses
+
+
+Live under the trees 
+in secure gated neighbourhood.
+
+
+
+
+
+
+Large 4BHK 
+Masterplan
+Flexi-Plan
+XLarge 4BHK
+Amenities
+Infrastructure
+Updates
+Contact Us
+
+
+
+
+
+
+Large 4BHK
+2984sft & 2988sft 
+Standalone Houses
+
+Unlock 25 secrets of the Large 4BHK House
+[Unlock Now]
+
+- No Common Walls
+- A Standalone 4BHK House
+- Ground and 2 Floors
+- With Back Door Access
+
+Find out Secret price breakdown
+Real-time pricing & Availability
+
+Large 4BHK House
+Basic Price 2.0Cr
+[Download Details]
+
+Large 4BHK + Private Garden
+Basic Price 2.3Cr
+[Download Details]
+
+
+- Landscaped Terrace Garden
+- Hidden Utility Terrace
+- 2 Private Car Parks
+- 15 Floor Plan Configurations
+- Dedicated Servants Quarters
+
+
+
+
+
+
+It's not a row house?
+[Watch the ad film]
+
+
+
+
+
+
+Masterplan
+Minimised Concrete Footprint
+
+Unlock 25 secrets of the Masterplan
+[Unlock Now]
+
+- 2 years of design before execution
+- No concrete podium or Basement
+- 73% exposed natural soil
+- 1.2 acre wooden central park
+
+
+12 houses per acre, instead of 16 or 24 per acre.
+
+Assetz 
+Soul & Soil
+
+7 Acres
+124 Houses
+17.7 per acre
+3 & 4 BHK
+Row Houses
+
+
+LW 
+Secret Soil
+
+6.4 Acres
+77 Houses
+11.8 per acre
+4 BHK
+Standalone Houses
+
+
+GoyalCo 
+Alanoville
+
+5 Acres
+75 Houses
+15 per acre
+4 BHK
+Row Houses
+
+
+- Vehicular traffic hugs the peripheral wall
+- 15 elderly jackfruit trees
+- More than 50 elderly trees 
+
+
+
+
+
+
+
+15 fully grown Jackfruit trees?
+[Watch the ad film]- 
+
+
+
+
+
+
+
+The charm of a BDA Layout House without the hassles
+
+
+Unlock 15 secrets of buying a house vs building one
+[Unlock Now]
+
+
+- Upgradable underground infrastructure
+- Authorised pedestrian and vehicular access
+- Child friendly interior roads
+- Electrified boundary wall
+- Quiet streets with few neighbours
+- You don’t need to supervise the build
+
+
+
+
+It’s like an independent house?
+[Watch the ad film]
+
+
+
+
+
+FLEXI-PLAN
+
+
+Unlock 14 secrets of the Flexi-Plan
+[Unlock Now]
+
+
+- Convert Bedroom to Home Theater Room
+- Add the provision for an Optional Lift
+
+Upto 14 unique Flexi-Plan Layouts Available
+
+[looping screen video]
+
+Customise your house online
+[customise now]
+
+
+- Add a walk-in dressing room
+- Opt for a large island kitchen
+
+
+
+
+
+
+It has a hidden utility terrace?
+[Watch the ad film]
+
+
+
+
+
+
+Construction Status
+
+October 2015
+[Previous Months]
+
+Structure
+- Block A - 17th Floor in progress. Block B & C - Completed
+
+Masonry
+- Block A - 13th Floor in progress. Block B - 15th floor Completed. Block C - 16th floor Completed
+- STP Work in progress
+- Basement ramps structure in progress
+- GFRC work in the ducts in progress
+- Fire suppression sprinkler system is being installed
+- Plumbing-Down take line in shaft in progress
+
+[ Gallery ]
+
+
+
+
+
+Location
+
+Where is Secret Soil Located?
+[google maps]
+
+
+
+
+
+
+Contact us
+Get in touch with us
+
+Fill in your details and a
+Customer Service executive
+will call you back, Soon.
+
+Or Call
+96633 96979
+
+Full Name
+Email ID
+Mobile Number
+Contact Me
+
+
+Pick your house online now.
+Use our Real-time Pricing Engine. 
+[Download Prices]
+
+
+
+
+
+WHO IS LIVING WALLS ?
+
+Living Walls is the brand name we use for all real estate projects undertaken by VDB Infra & Realty Pvt. Ltd....that's us...a sister concern of our parent company, the infrastructure conglomerate; VDB Projects Pvt. Ltd. has been involved in infrastructure development namely roads & highways, storm water drains across South India since it's inception.
+
+We ventured into Real Estate Development in 2011. By virtue of lineage, we inherently adhere to the same high standards of quality and commitment that VDB has been known for over the years. We're a young company at heart, fueled by an urge to create relevant living experiences, the kind that we see ourselves living in.
+
+
+
+Corporate Office
+
+VDB Infra & Realty Pvt. Ltd.
+VDB House, 842/A
+100ft Road, Indiranagar
+Bangalore - 5600 38
+
+
+Site Map
+
+–  Large 4BHK 
+–  Masterplan
+–  Flexi-Plan
+–  XLarge 4BHK
+–  Amenities
+
+–  Infrastructure
+–  Updates
+–  Contact us
+–  Location
+–  Pricing
+
+
+Disclaimer
+
+By using or accessing the Website you agree with the Disclaimer without any qualification or limitation. The Company reserves the right to terminate, revoke, modify, alter, add and delete any one or more of the terms and conditions of the website. The Company shall be under no obligation to notify the visitor of the amendment to the terms and conditions and the visitor shall be bound by such amended terms and conditions.
+
+The visuals and information contained herein marked as "artistic impression" are artistic impressions being indicative in nature and are for general information purposes only. The visuals contained marked as "generic image" and other visuals /image /photographs are general images and do not have any correlation with the project.
+
+The imagery used on the website may not represent actuals or may be indicative of style only. Photographs of interiors, surrounding views and location may not represent actuals or may have been digitally enhanced or altered. These photographs may not represent actuals or may be indicative only. Computer generated images, walkthroughs and render images are the artist's impression and are an indicative of the actual designs.
+
+No information given on this Website creates a warranty or expand the scope of any warranty that cannot be disclaimed under the applicable laws. The information on this website is presented as general information and no representation or warranty is expressly or impliedly given as to its accuracy. Any interested party should verify all the information including designs, plans, specifications, facilities, features, payment schedules, terms of sales etc independently with the Company prior to concluding any decision for buying in any of the project.
+
+While enough care is taken by the Company to ensure that information in the website are up to date, accurate and correct, the readers/ users are requested to make an independent enquiry with the Company before relying upon the same. Nothing on the website should be misconstrued as advertising, marketing, booking, selling or an offer for sale or invitation to purchase a unit in any project by the Company. The Company is not responsible for the consequences of any action taken by the viewer relying on such material/ information on this website without independently verifying with the Company.
+
+
+
 
 
 <?php require_once __DIR__ . '/../inc/below.php'; ?>
