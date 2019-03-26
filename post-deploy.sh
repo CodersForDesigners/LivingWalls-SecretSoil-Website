8056e1ea83b8fcbc6286cb83c28b0272b47eb6b8
adi <adityabhat@lazaro.in>
2019-03-26 18:35:31 +0530

    Deployment : Add a post-deployment script

---
+++ post-deploy.sh
@@ -0,0 +1,25 @@
+
+#! /bin/bash
+
+while getopts "p:" opt; do
+	case ${opt} in
+		p )
+			PROJECT_DIR=${OPTARG}
+			;;
+	esac
+done
+
+# Establish symbolic links for the following directories:
+# the media folder
+rm -rf media
+ln -s ../media/${PROJECT_DIR} media
+# the cms folder
+rm -rf cms
+ln -s ../${PROJECT_DIR}-cms cms
+# # the BFS theme folder for the CMS
+cd ../${PROJECT_DIR}-cms/wp-content/themes
+rm bfs
+ln -s ../../../${PROJECT_DIR}/resources/wordpress-theme bfs
+cd ../..
+wp --allow-root theme activate bfs
+cd ../${PROJECT_DIR}
