fbea285115a0aaedd091d11acdc0116a5327a21b
adi <adityabhat@lazaro.in>
2019-03-26 17:52:07 +0530

    Resource Linking : Use a `base` tag ( if needed )

--- inc/head.php
+++ inc/head.php
@@ -14,6 +14,20 @@ $siteTitle = getContent( 'Brownie As Fudge', 'site_title' );
 $pageUrl = $siteUrl . $_SERVER[ 'REQUEST_URI' ];
 $pageTitle = getCurrentPageTitle( $links, $siteTitle );
 
+
+/*
+ * Figure out the base URL
+*/
+$urlFragments = preg_split( '/\//', $_SERVER[ 'REQUEST_URI' ] );
+$baseURL = '';
+foreach ( $urlFragments as $fragment ) {
+	if ( ! empty( $fragment ) ) {
+		$baseURL = '/' . $fragment . '/';
+		break;
+	}
+}
+
+
 ?>
 
 <head>
@@ -25,7 +39,9 @@ $pageTitle = getCurrentPageTitle( $links, $siteTitle );
 	<title><?php echo $pageTitle ?></title>
 
 
-	<base href="/">
+	<?php if ( ! empty( $baseURL ) ) : ?>
+		<base href="<?php echo $baseURL ?>">
+	<?php endif; ?>
 
 	<!--
 	*
