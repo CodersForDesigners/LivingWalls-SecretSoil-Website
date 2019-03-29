3bd6de8091d8c0c3379479aa7f804a0c96b31ec4
adi <adityabhat@lazaro.in>
2019-03-29 17:11:28 +0530

    Video Embeds : Update the infrastructure to account for things like video covers
    Better code organisation.

--- js/modules/video_embed.js
+++ js/modules/video_embed.js
@@ -22,19 +22,15 @@ That URL is then plugged into the iframe element asynchronously.
 // copies the URL from its "data-src" attribute
 // and attaches to the `src` attribute of the iframe element that is a descendant of it
 function setVideoEmbed ( domEl ) {
-
 	$el = $( domEl );
 	var src = $el.data( 'src' );
 	$el.find( 'iframe' ).attr( 'src', src );
-
 }
 
 // takes a DOM element as input
 // removes the `src` attribute of the iframe element that is a descendant of it
 function unsetVideoEmbed ( domEl ) {
-
 	$( domEl ).find( 'iframe' ).attr( 'src', '' );
-
 }
 
 // initializes the video embeds asynchronously after a timeout
@@ -49,11 +45,11 @@ function initVideoEmbeds () {
 		} );
 	}, 3000);
 
-	// If there isn't a background YouTube embed, move on
-	if ( $( ".js_embed_bg_yt" ).length == 0 )
+	// If there isn't a requirement for the YouTube API, move on
+	if ( $( ".js_api_yt" ).length == 0 )
 		return;
 
-	// If there is a background YouTube embed, then
+	// If there is a requirement for the YouTube API, then
 	// 1. Load the YouTube API library (asynchronously)
 	var scriptElement = document.createElement( "script" );
 	scriptElement.src = "https://www.youtube.com/iframe_api";
@@ -61,31 +57,10 @@ function initVideoEmbeds () {
 
 	// 2. Setup the YouTube video, its playback options and hooks event handling
 	function onYouTubeIframeAPIReady () {
-		new YT.Player( "youtube_video_embed", {
-			events: {
-				onReady: onPlayerReady,
-				onStateChange: onPlayerStateChange
-			}
-		} );
+		$( document ).trigger( "ready/api/youtube" );
 	}
 	// This function needs to exposed as a global
 	window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
-
-	// When the YouTube video player is ready, this function is run
-	function onPlayerReady ( event ) {
-		// We wait for a moment and then play the video.
-		// This is so that it autoplays on mobile devices
-		setTimeout( function () {
-		  	event.target.playVideo();
-		}, 1500 )
-	}
-
-	// Whenever the YouTube video player's state changes, this function is run
-	function onPlayerStateChange ( event ) {
-		// console.log( event.data )
-		if ( event.data == YT.PlayerState.ENDED )
-			event.target.seekTo( 0 )
-	}
 
 }
 
@@ -97,5 +72,53 @@ $( function () {
 	// initializes and loads the video embeds on **asynchronous** page loads
 	// hooks onto a custom event that is emitted, "page::load"
 	$( document ).on( "page::load", initVideoEmbeds );
+
+	// The Ad videos
+	$( document ).on( "ready/api/youtube", function ( event ) {
+
+		// When the YouTube video player is ready, this function is run
+		function onPlayerReady ( event ) {
+			var $videoCover = $( event.target.a ).parent().find( ".js_video_cover" );
+			$videoCover.addClass( "fade-in" );
+		}
+
+		// Whenever the YouTube video player's state changes, this function is run
+		function onPlayerStateChange ( event ) {
+
+			var domVideoPlayer = event.target.a;
+			var $videoPlayer = $( domVideoPlayer );
+			var $videoCover = $videoPlayer.parent().find( ".js_video_cover" );
+
+			if ( event.data == YT.PlayerState.PLAYING ) {
+				if ( $videoCover.hasClass( "js_paused" ) ) {
+					$videoCover.removeClass( "js_paused" );
+					return;
+				}
+				$videoPlayer.addClass( "fade-in" );
+				$videoCover.removeClass( "fade-in" );
+			}
+			if ( event.data == YT.PlayerState.ENDED ) {
+				// event.target.seekTo( event.target.getDuration() - 0.5 );
+				// event.target.pauseVideo();
+				$videoPlayer.removeClass( "fade-in" );
+				$videoCover.addClass( "fade-in pointer js_paused" );
+				setTimeout( function () {
+					event.target.seekTo( 0 );
+					event.target.pauseVideo();
+					$videoCover.removeClass( "pointer" );
+				}, 300 );
+			}
+		}
+
+		$( ".js_api_yt" ).each( function ( _i, domYTIframe ) {
+			new YT.Player( domYTIframe, {
+				events: {
+					onReady: onPlayerReady,
+					onStateChange: onPlayerStateChange
+				}
+			} );
+		} );
+
+	} );
 
 } );
