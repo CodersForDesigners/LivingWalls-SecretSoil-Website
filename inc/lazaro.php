29f61c3c1350c3bf6bb667bdbda987ad51417052
adi <adityabhat@lazaro.in>
2019-04-02 09:52:06 +0530

    Content : Update the Lazaro tagline on the footer

--- inc/lazaro.php
+++ inc/lazaro.php
@@ -3,7 +3,7 @@
 <section class="lazaro-section">
 	<div class="container">
 		<a href="http://lazaro.in/" target="_blank" class="lazaro-link ga_lazaro_click">
-			<strong>www.Lazaro.in</strong> — <span>Communication Strategy and Implementation</span>
+			<strong>www.Lazaro.in</strong> — <span>Marketing Strategy and Implementation</span>
 		</a>
 	</div>
 </section>
