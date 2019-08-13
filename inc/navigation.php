988c98d7959f275ac74bded2682553b768d9cd11
adi <adityabhat@lazaro.in>
2019-08-13 14:58:12 +0530

    Navigation : Consolidate all the navigation section markup

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,23 +1,41 @@
 <?php
 
+if ( empty( $baseURL ) )
+	$navigationBaseURL = '/';
+else
+	$navigationBaseURL = $navigationBaseURL
 ?>
 
-<!-- Header Section -->
-<section class="header-section">
-	<div class="container">
-		<div class="header row">
-			<div class="columns small-3">
-				<a class="logo" href="/">
-					<img src="media/logo.svg<?php echo $ver ?>">
+<section class="navigation-section">
+	<div class="menu-1 row js_primary_nav">
+		<div class="container">
+			<div class="columns small-12 clearfix">
+				<a class="logo" href="<?= $navigationBaseURL ?>">
+					<img src="media/ss-logo-long-green-light.svg<?php echo $ver ?>">
 				</a>
-			</div>
-			<div class="text-right columns small-9">
-				<div class="navigation inline">
-					<?php foreach ( $links as $link ) : ?>
-						<a class="button js_nav_button" href="<?php echo $link[ 'slug' ] ?>" target="<?php echo $link[ 'new_tab' ] ? '_blank' : '_self' ?>"><?php echo $link[ 'label' ] ?></a>
-					<?php endforeach; ?>
+				<div class="menu-toggle js_primary_nav_menu_toggle float-right show-for-tablet" tabindex="-1">
+					<span class="h5 text-uppercase inline-middle">Menu</span>
+					<span class="h4 inline-middle"> &#9776;</span>
+				</div>
+				<div class="link-tray js_primary_nav_link_tray"><!-- To Toggle Reveal: Add show class for small & medium Breakpoints -->
+					<div class="close-link-tray js_close_nav_link_tray"></div>
+					<a class="link h5 text-uppercase" href="<?= $navigationBaseURL ?>#contact">Contact us</a>
+					<a class="link h5 text-uppercase" href="<?= $navigationBaseURL ?>#location">Location</a>
+					<a class="link h5 text-uppercase" href="tel:+919663396979">Call 9663396979</a>
+					<a class="link h5 text-uppercase fill-green" href="<?= $navigationBaseURL ?>pricing">Real-time Pricing</a>
 				</div>
 			</div>
 		</div>
 	</div>
-</section> <!-- END : Header Section -->
+	<div class="menu-2 row fill-dark js_secondary_nav">
+		<div class="link-tray">
+			<a class="link label text-uppercase" href="<?= $navigationBaseURL ?>#large-4bhk">Large 4BHK</a>
+			<a class="link label text-uppercase" href="<?= $navigationBaseURL ?>#masterplan">Masterplan</a>
+			<a class="link label text-uppercase" href="<?= $navigationBaseURL ?>#flexiplan">Flexi-Plan</a>
+			<a class="link label text-uppercase" href="<?= $navigationBaseURL ?>#xlarge-4bhk">XLarge 4BHK</a>
+			<a class="link label text-uppercase" href="<?= $navigationBaseURL ?>#amenities">Amenities</a>
+			<a class="link label text-uppercase" href="<?= $navigationBaseURL ?>#construction">Updates</a>
+			<a class="link label text-uppercase" href="<?= $navigationBaseURL ?>#contact">Contact Us</a>
+		</div>
+	</div>
+</section>
--- pages/construction-single.php
+++ pages/construction-single.php
@@ -47,39 +47,7 @@ $gallery = array_slice( $gallery, 1 );
 
 
 <!-- Navigation Section : Menu 1 -->
-<section class="navigation-section">
-	<div class="menu-1 row js_primary_nav">
-		<div class="container">
-			<div class="columns small-12 clearfix">
-				<a class="logo" href="">
-					<img src="media/ss-logo-long-green-light.svg<?php echo $ver ?>">
-				</a>
-				<div class="menu-toggle js_primary_nav_menu_toggle float-right show-for-tablet" tabindex="-1">
-					<span class="h5 text-uppercase inline-middle">Menu</span>
-					<span class="h4 inline-middle"> &#9776;</span>
-				</div>
-				<div class="link-tray js_primary_nav_link_tray"><!-- To Toggle Reveal: Add show class for small & medium Breakpoints -->
-					<div class="close-link-tray js_close_nav_link_tray"></div>
-					<a class="link h5 text-uppercase" href="#contact">Contact us</a>
-					<a class="link h5 text-uppercase" href="#location">Location</a>
-					<a class="link h5 text-uppercase" href="tel:+919663396979">Call 9663396979</a>
-					<a class="link h5 text-uppercase fill-green" href="pricing">Real-time Pricing</a>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="menu-2 row fill-dark js_secondary_nav">
-		<div class="link-tray">
-			<a class="link label text-uppercase" href="#large-4bhk">Large 4BHK</a>
-			<a class="link label text-uppercase" href="#masterplan">Masterplan</a>
-			<a class="link label text-uppercase" href="#flexiplan">Flexi-Plan</a>
-			<a class="link label text-uppercase" href="#xlarge-4bhk">XLarge 4BHK</a>
-			<a class="link label text-uppercase" href="#amenities">Amenities</a>
-			<a class="link label text-uppercase" href="#construction">Updates</a>
-			<a class="link label text-uppercase" href="#contact">Contact Us</a>
-		</div>
-	</div>
-</section>
+<?php require_once __DIR__ . '/../inc/navigation.php' ?>
 <!-- END: Navigation Section -->
 
 
